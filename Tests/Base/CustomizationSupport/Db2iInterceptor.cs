e908d2b292f36ed6c2825e951a3a5a3586a848d9
nsarris <nsarris@gmail.com>
2020-11-16 19:02:47 +0200

    Fixed a few tests and Interceptor clean up.

--- Tests/Base/CustomizationSupport/Db2iInterceptor.cs
+++ Tests/Base/CustomizationSupport/Db2iInterceptor.cs
@@ -35,65 +35,7 @@ namespace Tests
 			return contexts;
 		}
 
-
-		public override IEnumerable<string> GetInsertOrUpdateUnsupportedProviders(IEnumerable<string> providers)
-		{
-			return providers.Concat(TestProvNameDb2i.GetProviders(TestProvNameDb2i.All_54));
-		}
-
-		public override IEnumerable<string> GetMergeUnsupportedProviders(IEnumerable<string> providers)
-		{
-			return providers.Concat(TestProvNameDb2i.GetProviders(TestProvNameDb2i.All_54));
-		}
-
-		public override IEnumerable<string> GetIdentityInsertMergeSupportedProviders(IEnumerable<string> providers)
-		{
-			return base.GetIdentityInsertMergeSupportedProviders(providers);
-		}
-
 		private static (string className, string methodName) ExtractMethod(IMethodInfo testMethod)
 			=> (testMethod.TypeInfo.Name, testMethod.Name);
-	}
-
-
-	public static class ProviderSupport
-	{
-		public static IEnumerable<string> GetSupportedProviders(IEnumerable<string> providers)
-		{
-			return TestProvNameDb2i.GetAll();
-		}
-
-		public static IEnumerable<string> GetInsertOrUpdateUnsupportedProviders(IEnumerable<string> providers)
-		{
-			return providers.Concat(TestProvNameDb2i.GetProviders(TestProvNameDb2i.All_54));
-		}
-
-		public static IEnumerable<string> GetMergeUnsupportedProviders(IEnumerable<string> providers)
-		{
-			return providers.Concat(TestProvNameDb2i.GetProviders(TestProvNameDb2i.All_54));
-		}
-
-		public static IEnumerable<string> GetIdentityInsertMergeSupportedProviders(IEnumerable<string> providers)
-		{
-			return providers;
-		}
-	}
-
-	public static class ProviderTestSupport
-	{
-		public static IEnumerable<string> InterceptSupportedProviders(IEnumerable<string> providers)
-		{
-			return providers.Concat(TestProvNameDb2i.GetProviders(providers));
-		}
-
-		public static IEnumerable<string> InterceptSupportedTestProviders(IMethodInfo testMethod, IEnumerable<string> providers)
-		{
-			//Filter out specific tests
-			if (testMethod.TypeInfo.Name == "SchemaProviderTests"
-				&& testMethod.Name == "DB2Test")
-				return Enumerable.Empty<string>();
-
-			return providers;
-		}
 	}
 }
--- Tests/Linq/Custom/DB2iSeriesTests.DataExtensions.cs
+++ Tests/Linq/Custom/DB2iSeriesTests.DataExtensions.cs
@@ -17,8 +17,8 @@ namespace Tests.DataProvider
 
 			using (var conn = new DataConnection())
 			{
-				Assert.That(conn.Execute<byte[]>("VALUES (@p)", new { p = arr1 }), Is.EqualTo(arr1));
-				Assert.That(conn.Execute<byte[]>("VALUES (@p)", new { p = arr2 }), Is.EqualTo(arr2));
+				Assert.That(conn.Execute<byte[]>("VALUES (CAST(? AS BINARY(2)))", new { p = arr1 }), Is.EqualTo(arr1));
+				Assert.That(conn.Execute<byte[]>("VALUES (CAST(? AS BINARY(1)))", new { p = arr2 }), Is.EqualTo(arr2));
 			}
 		}
 
@@ -27,7 +27,7 @@ namespace Tests.DataProvider
 		{
 			using (var conn = new DataConnection())
 			{
-				Assert.That(conn.Execute<int>("VALUES (@p)", new { p = 1 }), Is.EqualTo(1));
+				Assert.That(conn.Execute<int>("VALUES (CAST(? AS INTEGER))", new { p = 1 }), Is.EqualTo(1));
 			}
 		}
 
@@ -37,12 +37,11 @@ namespace Tests.DataProvider
 			using (var conn = new DataConnection())
 			{
 				var res = conn.Execute<string>(
-					"VALUES (@p)",
-					new
-					{
-						p  = new DataParameter { DataType = DataType.VarChar, Value = "123" },
-						p1 = 1
-					});
+				"VALUES (CAST(? AS VARCHAR(3)))",
+				new
+				{
+					p = new DataParameter { DataType = DataType.VarChar, Value = "123" }
+				});
 
 				Assert.That(res, Is.EqualTo("123"));
 			}
@@ -56,7 +55,7 @@ namespace Tests.DataProvider
 				conn.InlineParameters = true;
 				var sql = conn.Person.Where(p => p.ID == 1).Select(p => p.Name).Take(1).ToString()!;
 				sql = string.Join(Environment.NewLine, sql.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries)
-					.Where(line => !line.StartsWith("--")));
+				.Where(line => !line.StartsWith("--")));
 				var res = conn.Execute<string>(sql);
 
 				Assert.That(res, Is.EqualTo("John"));
@@ -69,12 +68,12 @@ namespace Tests.DataProvider
 			using (var conn = new TestDataConnection(context))
 			{
 				var result = conn.Person.Where(p => p.ID == 1).Select(p => new { p.ID, p.Name })
-					.Take(1)
-					.ToArray();
+				.Take(1)
+				.ToArray();
 
 				var expected = Person.Where(p => p.ID == 1).Select(p => new { p.ID, p.Name })
-					.Take(1)
-					.ToArray();
+				.Take(1)
+				.ToArray();
 
 				AreEqual(expected, result);
 			}
@@ -86,12 +85,11 @@ namespace Tests.DataProvider
 			using (var conn = new DataConnection())
 			{
 				Assert.That(conn.Execute<string>(
-					"VALUES (@p)",
-					new
-					{
-						p1 = new DataParameter { Name = "p", DataType = DataType.Char, Value = "123" },
-						p2 = 1
-					}), Is.EqualTo("123"));
+				"VALUES (CAST(? AS CHAR(3)))",
+				new
+				{
+					p1 = new DataParameter { Name = "p", DataType = DataType.Char, Value = "123" }
+				}), Is.EqualTo("123"));
 			}
 		}
 	}
