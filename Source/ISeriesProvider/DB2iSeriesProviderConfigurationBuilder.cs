a450e2039aa167edf258058ce40da529499ea36c
nsarris <nsarris@gmail.com>
2021-04-18 20:58:56 +0300

    Added LinqToDbConnectionOptionsBuilderExtensions (UseDB2iSeris)

---
+++ Source/ISeriesProvider/DB2iSeriesProviderConfigurationBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LinqToDB.DataProvider.DB2iSeries
+{
+	public class DB2iSeriesProviderConfigurationBuilder
+	{
+		internal DB2iSeriesProviderType ProviderType { get; set; } = DB2iSeriesProviderOptions.Defaults.ProviderType;
+		internal DB2iSeriesVersion Version { get; set; } = DB2iSeriesProviderOptions.Defaults.Version;
+		internal DB2iSeriesMappingOptions MappingOptions { get; set; } = DB2iSeriesMappingOptions.Default;
+
+		public DB2iSeriesProviderConfigurationBuilder WithProviderType(DB2iSeriesProviderType providerType)
+		{
+			ProviderType = providerType;
+			return this;
+		}
+
+		public DB2iSeriesProviderConfigurationBuilder WithVersion(DB2iSeriesVersion version)
+		{
+			Version = version;
+			return this;
+		}
+		public DB2iSeriesProviderConfigurationBuilder WithMappingOptions(DB2iSeriesMappingOptions mappingOptions)
+		{
+			MappingOptions = mappingOptions ?? throw new ArgumentNullException(nameof(mappingOptions));
+			return this;
+		}
+	}
+}
